78ca7ee72f3f7e9297dc52e02b010b5b288526cc
muchfahmiarif <fahmiarif96@gmail.com>
2023-05-18 21:31:39 +0700

    testing api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import {} from "react";
+import { useEffect } from "react";
 import "./App.css";
+import { fetchDataFromApi } from "./utils/api";
 
 function App() {
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = async () => {
+    fetchDataFromApi("/movie/popular").then((res) => console.log(res));
+  };
+
   return <>App</>;
 }
 
--- src/utils/api.js
+++ src/utils/api.js
@@ -10,7 +10,7 @@ const headers = {
 export const fetchDataFromApi = async (url, params) => {
   try {
     const { data } = await axios.get(`${BASE_URL}${url}`, {
-      header: headers,
+      headers: headers,
       params: params,
     });
     return data;
