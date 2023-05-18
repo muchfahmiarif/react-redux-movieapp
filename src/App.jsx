afc26907fa31fd8a8dc69c6cd00b27a5e7c70a9a
muchfahmiarif <fahmiarif96@gmail.com>
2023-05-18 23:18:07 +0700

    add redux on app.jsx and testing redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useEffect } from "react";
 import "./App.css";
 import { fetchDataFromApi } from "./utils/api";
+import { useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
   useEffect(() => {
     apiTesting();
   }, []);
 
   const apiTesting = async () => {
-    fetchDataFromApi("/movie/popular").then((res) => console.log(res));
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+      dispatch(getApiConfiguration(res));
+    });
   };
 
   return <>App</>;
