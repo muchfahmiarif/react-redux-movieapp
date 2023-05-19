e7985353aafff92b77f7a91edc2f5ce2253e1527
muchfahmiarif <fahmiarif96@gmail.com>
2023-05-19 08:17:35 +0700

    get total pages from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
 import { useEffect } from "react";
 import "./App.css";
 import { fetchDataFromApi } from "./utils/api";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration } from "./store/homeSlice";
 
 function App() {
   const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+  console.log(url);
 
   useEffect(() => {
     apiTesting();
   }, []);
 
   const apiTesting = async () => {
-    fetchDataFromApi("/movie/popular").then((res) => {
+    // contoh tidak menggunanakan async await
+    await fetchDataFromApi("/movie/popular").then((res) => {
       console.log(res);
       dispatch(getApiConfiguration(res));
     });
   };
 
-  return <>App</>;
+  return (
+    <>
+      App
+      {url?.total_pages}
+    </>
+  );
 }
 
 export default App;
