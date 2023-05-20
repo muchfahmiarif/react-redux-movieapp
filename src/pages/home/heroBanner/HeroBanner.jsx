65cf45fa57e619b80c81a6b81a3601ffe3dc685a
muchfahmiarif <fahmiarif96@gmail.com>
2023-05-20 18:39:57 +0700

    add input on banner for search and show on url with dinamic url

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./HeroBanner.scss";
+import { useNavigate } from "react-router-dom";
 
 const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  const searchQueryHandler = (e) => {
+    // 13 adalah kode untuk tombol enter
+    if (e.keyCode === 13 && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
+
+  return (
+    <div>
+      <div className="heroBanner">
+        <div className="wrapper">
+          <div className="heroBannerContent">
+            <span className="title">Welcome</span>
+            <span className="subTitle">Millions of movies, TV shows and people to discover. Elxplore now.</span>
+            <div className="searchInput">
+              <input
+                type="text"
+                placeholder="Search for a movice or tv show..."
+                onKeyUp={searchQueryHandler}
+                onChange={(e) => setQuery(e.target.value)}
+              />
+              <button>Search</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroBanner;
