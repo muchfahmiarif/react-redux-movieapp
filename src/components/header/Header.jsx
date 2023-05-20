926c718b340f2aa1269c1d8862cdcb990f62baab
muchfahmiarif <fahmiarif96@gmail.com>
2023-05-21 06:16:44 +0700

    add header jsx and style for header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { HiOutlineSearch } from "react-icons/hi";
+import { SlMenu } from "react-icons/sl";
+import { VscChromeClose } from "react-icons/vsc";
+import { useNavigate, useLocation } from "react-router-dom";
+
 import "./Header.scss";
 
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import logo from "../../assets/movix-logo.svg";
+
 const Header = () => {
-  return <div>Header</div>;
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  return (
+    <div className="header">
+      <ContentWrapper>
+        <div className="logo">
+          <img src={logo} alt="TMDB" />
+        </div>
+        <ul className="menuItems">
+          <li className="menuItem">Movies</li>
+          <li className="menuItem">TV Shows</li>
+          <li className="menuItem">
+            <HiOutlineSearch />
+          </li>
+        </ul>
+        <div className="mobileMenuItems">
+          <HiOutlineSearch />
+          <SlMenu />
+        </div>
+      </ContentWrapper>
+    </div>
+  );
 };
 
 export default Header;
