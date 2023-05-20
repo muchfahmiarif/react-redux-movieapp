7054237085a3749a38ba194270cc18e0a62c4cc6
muchfahmiarif <fahmiarif96@gmail.com>
2023-05-20 15:51:43 +0700

    add components and react router

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,15 @@ import "./App.css";
 import { fetchDataFromApi } from "./utils/api";
 import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration } from "./store/homeSlice";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
 
 function App() {
   const dispatch = useDispatch();
@@ -22,10 +31,11 @@ function App() {
   };
 
   return (
-    <>
-      App
-      {url?.total_pages}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
