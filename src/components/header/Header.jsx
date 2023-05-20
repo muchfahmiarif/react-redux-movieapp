2444385f8db57b25948c987a8def7a10981b77bb
muchfahmiarif <fahmiarif96@gmail.com>
2023-05-21 06:59:41 +0700

    add mobile view on mobile screen

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
   };
 
   return (
-    <div className="header">
+    <div className={`header ${mobileMenu ? "mobileView" : ""}`}>
       <ContentWrapper>
         <div className="logo">
           <img src={logo} alt="TMDB" />
