0b08c87fdc3a5545bfd40318e152dbf3bf39ec4c
muchfahmiarif <fahmiarif96@gmail.com>
2023-05-21 13:43:15 +0700

    add switchtabs component and import on trending carousel

---
+++ src/components/switchTabs/SwitchTabs.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./SwitchTabs.scss";
+
+const SwitchTabs = () => {
+  return <div>SwitchTabs</div>;
+};
+
+export default SwitchTabs;
--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 
 const Trending = () => {
   return (
     <div className="carouselSection">
       <ContentWrapper>
         <span className="carouselTitle">Trending</span>
+        <SwitchTabs />
       </ContentWrapper>
     </div>
   );
