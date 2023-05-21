a2e472cf046aee461db68100a8d266f96c886953
muchfahmiarif <fahmiarif96@gmail.com>
2023-05-21 19:03:44 +0700

    `carousel` add components carousel and css carousel

---
+++ src/components/carousel/Carousel.jsx
@@ -0,0 +1,17 @@
+import React, { useRef } from "react";
+import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import dayjs from "dayjs";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import Img from "../lazyLoadImage/Img";
+import PosterFallback from "../../assets/no-poster.png";
+
+import "./Carousel.scss";
+
+const Carousel = ({ data, loading }) => {
+  return <div>Carousel</div>;
+};
+
+export default Carousel;
--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -2,10 +2,11 @@ import { useState } from "react";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 import useFetch from "../../../hooks/useFetch";
+import Carousel from "../../../components/carousel/Carousel";
 
 const Trending = () => {
   const [endPoint, setEndPoint] = useState("day");
-  const { data, loading } = useFetch(`/trending/all/${endPoint}`);
+  const { data, loading } = useFetch(`/trending/all/${endPoint}`); // docs : https://developer.themoviedb.org/reference/trending-all
 
   const onTabChange = (tab) => {
     setEndPoint(tab.toLowerCase());
@@ -17,6 +18,7 @@ const Trending = () => {
         <span className="carouselTitle">Trending</span>
         <SwitchTabs onTabChange={onTabChange} data={["Day", "Week"]} />
       </ContentWrapper>
+      <Carousel data={data?.result} loading={loading} />
     </div>
   );
 };
