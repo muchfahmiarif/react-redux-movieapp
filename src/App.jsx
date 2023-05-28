6599a9375760a3b67cf2ed69d2050418f94a0f26
muchfahmiarif <fahmiarif96@gmail.com>
2023-05-29 00:16:47 +0700

    `genre` show all genre

--- src/App.jsx
+++ src/App.jsx
@@ -48,6 +48,10 @@ function App() {
 
     const data = await Promise.all(promises);
     console.log(data);
+    data.map(({ genres }) => {
+      return genres.map((item) => (allGenres[item.id] = item));
+    });
+    console.log(allGenres);
   };
 
   return (
